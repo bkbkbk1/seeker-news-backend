090be8a75863cd14ecb5acf2e059c2368ae8996b
bkbkbk1 <91306039+bkbkbk1@users.noreply.github.com>
2025-10-08 21:47:45 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Configuration for Seeker News Backend"""
 
 # X (Twitter) accounts to monitor
- MONITORED_ACCOUNTS = [
+MONITORED_ACCOUNTS = [
       "solanamobile",
       "aeyakovenko",
       "solana_devs",
@@ -19,4 +19,5 @@ SEEKER_KEYWORDS = [
 
 # How many recent tweets to fetch per account
 TWEETS_PER_ACCOUNT = 10
+
 
